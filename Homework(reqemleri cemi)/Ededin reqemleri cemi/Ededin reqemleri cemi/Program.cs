f53a19ddd895228b37856ba0e5f35c4b12d0a321
IamMasudMirzazada <masudjm@code.edu.az>
2021-11-28 11:31:08 -0800

    task

---
+++ Homework(reqemleri cemi)/Ededin reqemleri cemi/Ededin reqemleri cemi/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Ededin_reqemleri_cemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ededin reqemleri cemini hesablayan      (TASK 1)
+            //string eded = Console.ReadLine();
+            //int num = int.Parse(eded);
+
+            //int rem;
+            //int sum = 0;
+            //while (num > 0)
+            //{
+            //    rem = num % 10;
+            //    sum = sum + rem;
+            //    num = num / 10;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Sade ve Murekkeb            (TASK2)
+            //string reqem = Console.ReadLine();
+            //int n = int.Parse(reqem);
+
+            //bool result = false;
+
+            //if (n>1)
+            //{
+            //    for (int i = 2; i < n; i++)
+            //    {
+            //        if (n % i == 0)
+            //        {
+            //            result = true;
+            //        }
+
+            //    }
+            //    if (result == true)
+            //    {
+            //        Console.WriteLine("murekkeb");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sadedir");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ne sade nede murekkeb ededir");
+            //}
+
+            #endregion
+
+            #region TASK FESIL VE AY          (TASK3)
+            //string month = Console.ReadLine();
+            //int ay = int.Parse(month);
+            //switch (ay)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("Daxil etdiyiniz reqem QIŞ fesline uyğundur");
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Daxil etdiyiniz reqem YAZ fesline uyğundur");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Daxil etdiyiniz  reqem YAY fesline uyğundur");
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Daxil etdiyiniz reqem PAYIZ fesline uyğundur");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Daxil etdiyiniz reqem hec bir  fesil-e aid deyil");
+            //        break;
+            //}
+            #endregion
+        }
+    }
+}
